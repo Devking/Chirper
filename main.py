060dbcb69bc472c2af74a3e45f6819e7f600711c
Wells Santo <wls254@nyu.edu>
2016-02-22 19:05:47 -0500

    can pick up basic login

--- main.py
+++ main.py
@@ -7,12 +7,7 @@ app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def hello():
 	if request.method == 'POST':
-		# print request.form["form_data"]
-		# return 'got a post'
-		# Generate the template page making use of the form data!
-		return render_template("verify.html", responsetext=request.form["form_data"])
-	# Instead of having inline HTML, we can just load a html page
-	# Our HTML form should have a name -- so we get that variable back in the POST
+		return render_template("verify.html", username=request.form["username"], password=request.form["password"])
 	return render_template("login.html")
 
 app.run("127.0.0.1", 8000, debug=True)
