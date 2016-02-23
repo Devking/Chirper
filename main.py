30911cbf07bfcbd0ccdd4979b4b0fccb2c93b777
Wells Santo <theonlydevking@gmail.com>
2016-02-23 00:14:24 -0500

    added manifest file

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
 # Wells Santo and Patrick Kingchatchaval
 
+# import functionality from the Flask package
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+# import in-memory user information
+from manifest import users
 
-# Currently this is just for debugging
-# This information should go in a text file or database
-chirps = [
-			{
-				'author': 'Gary',
-				'message': 'Hello'
-			},
-			{
-				'author': 'Kanye',
-				'message': 'Invest In Kanye West Ideas'
-			}
-		 ]
+app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def hello():
@@ -24,11 +15,11 @@ def hello():
 
 	if request.method == 'POST':
 
-		if request.form["username"] == 'admin' and request.form["password"] == 'pass':
+		username = request.form["username"]
+		if username in users.keys() and users[username]["password"] == request.form["password"]:
 			return render_template("verify.html",
-			                   	   username = request.form["username"],
-			                       password = request.form["password"],
-			                       chirps   = chirps)
+			                   	   username = username,
+			                       chirps   = users[username]["chirps"])
 
 		loginfail = True
 
---
+++ manifest.py
@@ -0,0 +1,31 @@
+# Currently this is just for debugging
+# This information should go in a text file or database
+
+users = {
+            'admin': {
+                'password': 'pass',
+                'chirps': [
+                            {
+                                'author': 'Gary',
+                                'message': 'Hello'
+                            },
+                            {
+                                'author': 'Kanye',
+                                'message': 'Invest In Kanye West Ideas'
+                            }
+                ]
+            },
+            'bob': {
+                'password': 'dole',
+                'chirps': [
+                            {
+                                'author': 'bob',
+                                'message': 'Hi'
+                            },
+                            {
+                                'author': 'Someone',
+                                'message': 'something'
+                            }
+                ]
+            }
+        }
