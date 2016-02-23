c83116e9b9362180d123299a3ff200aacb308d7c
Wells Santo <wls254@nyu.edu>
2016-02-22 19:45:15 -0500

    fake login credentials created

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 # Currently this is just for debugging
+# This information should go in a text file or database
 chirps = [
 			{
 				'author': 'Gary',
@@ -18,12 +19,19 @@ chirps = [
 
 @app.route('/', methods=['POST', 'GET'])
 def hello():
-	if request.method == 'POST':
-		return render_template("verify.html",
-			                   username=request.form["username"],
-			                   password=request.form["password"],
-			                   chirps=chirps)
 
-	return render_template("login.html")
+	loginfail = False
+
+	if request.method == 'POST':
+
+		if request.form["username"] == 'admin' and request.form["password"] == 'pass':
+			return render_template("verify.html",
+			                   	   username = request.form["username"],
+			                       password = request.form["password"],
+			                       chirps   = chirps)
+
+		loginfail = True
+
+	return render_template("login.html", loginfail = loginfail)
 
 app.run("127.0.0.1", 8000, debug=True)
