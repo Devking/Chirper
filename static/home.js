19454b01281489d723af3201962ade89597e6b0d
Wells Santo <theonlydevking@gmail.com>
2016-02-26 02:21:55 -0500

    added character count logic

--- static/home.js
+++ static/home.js
@@ -1,8 +1,18 @@
 $(function() {
 
+    $('.fn-chirp-field').focus();
+
     $('.fn-delete').click(function () {
         if (confirm('Do you really want to delete your account? There\'s no going back!'))
             window.location.href = '/deleteaccount'
     })
+
+    $('.fn-chirp-field').keydown(updateCharCount);
+    $('.fn-chirp-field').keyup(updateCharCount);
+
+    function updateCharCount() {
+        var count = $(this).val().length;
+        $('.fn-chirp-count').text(100 - count);
+    }
 
 });
